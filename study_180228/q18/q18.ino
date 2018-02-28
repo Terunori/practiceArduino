4bcc08f42b9d14d4b1427f2ab69b5e8ba7148952
teru <18109251+Terunori@users.noreply.github.com>
2018-02-28 18:59:59 +0900

    add comment

--- study_180228/q18/q18.ino
+++ study_180228/q18/q18.ino
@@ -14,16 +14,21 @@ void loop (){
     digitalWrite(Trig,LOW);
     delayMicroseconds(1);
     digitalWrite(Trig,HIGH);
+    // HIGHである時間は最低10μs必要
     delayMicroseconds(11);
+    // 40kHz(10usに8回パルスが出る)パルスを10us送信
     digitalWrite(Trig,LOW);
+    // パルスを送信し終わると自動でEcho pinをHighに
 
-    // 超音波が戻ってくるまでの時間を計測
+    // パルスを受け取るとEcho pinは自動でLOWに
+    // 「Echo pinがHIGHである時間」を計測することで, 超音波が戻ってくるまでの時間を計測
     Duration = pulseIn(Echo,HIGH);
 
     //距離計算
     if (Duration>0) {
         // ultrasonic speed is 340m/s = 0.034cm/us
         // 厳密には気温によって異なる
+        // 厳密にやりたいときは温度センサを併用
         Distance = Duration / 2;
         Distance = Distance * 340 * 100 /1000000; 
         Serial.print(Duration);
